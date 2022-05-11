b5e645eff54076d8231ab4ecf0112b2870966740
imran-salim <imransalim08@gmail.com>
2022-05-11 23:04:00 +1200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const cocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin';
+
+const App = () => {
+  // return <>Hello World</>;
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getCocktailWithAxios();
+  }, []);
+
+  const getCocktailWithAxios = async () => {
+    const response = await axios.get(cocktailUrl);
+    setUserData(response.data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Cocktail</h2>
       </header>
+      <div className="cocktail-container">
+        {JSON.stringify(userData)}
+      </div>
     </div>
   );
 }
